e588c9931b34f079dfcfb8d33d57fe0e015592aa
Your Name <renzo-lim@hotmail.com>
2017-11-01 08:51:44 +1000

    Nav links added to layout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < ApplicationController
   before_action :authenticate_user!
   def profile
+    @users = User.all
   end
 
   def dish
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'user/profile'
+
+  get 'user/dish'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
