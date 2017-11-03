bca6926d59a34d110ddc32a016b7b03c6594522d
Your Name <renzo-lim@hotmail.com>
2017-11-03 22:40:22 +1000

    Started Fixing Image Uploading on Heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,6 +4,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+  # include Cloudinary::CarrierWave
+  #
+  # process :convert => 'png'
+  # process :tags => ['post_picture']
+  #
+  # version :standard do
+  #   process :resize_to_fill => [100, 150, :north]
+  # end
+  #
+  # version :thumbnail do
+  #   resize_to_fit(50, 50)
+  # end
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
