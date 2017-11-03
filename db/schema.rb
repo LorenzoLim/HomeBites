bbb5ae52b1b4e6560362a1721cb3e9d9a8fbbef0
Your Name <renzo-lim@hotmail.com>
2017-11-03 15:58:26 +1000

    Image Migration Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103024008) do
+ActiveRecord::Schema.define(version: 20171103050538) do
 
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20171103024008) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "images"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
